f1d8c3ae0ea107cd4b2357f3b51457d2036ab3f4
OKkurva <yakymyshynya065@gmail.com>
2025-07-09 19:46:45 +0300

    zaliv_staroi_huyni

---
+++ main/script.js
@@ -0,0 +1,33 @@
+const slider = document.querySelectorAll('.slider-img');
+let index = 0;
+
+slider.forEach(s => s.addEventListener('click', slide));
+
+function slide(e){
+    this.classList.remove('active');
+    if(this.nextElementSibling){
+        this.nextElementSibling.classList.add('active');
+    } else {
+        slider[0].classList.add('active');
+    }
+}
+
+
+setInterval(()=> {
+    deactivate(slider[index]);
+    if (index < slider.length-1){
+        index++;
+    } else {
+        index = 0;
+    }
+    activate(slider[index]);
+    
+},5000)
+
+function activate(slide){
+    slide.classList.add('active');
+}
+
+function deactivate(slide){
+    slide.classList.remove('active');  
+}
